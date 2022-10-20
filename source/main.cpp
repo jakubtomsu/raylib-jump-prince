6ed0b1d383601eb51934242044b47ede4fb0ca4c
Jakub Tomšů <tomsuon@student.uzlabina.cz>
2022-10-20 13:31:59 +0200

    Tilemap concept

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,48 @@
 #include "raylib.h"
+#include <stdint.h>
+
+#define TILEMAP_SIZE_X 64
+#define TILEMAP_SIZE_Y 48
+#define TILE_PIXELS 16
+#define OUTSIDE_TILE_HORIZONTAL TILE_FULL
+#define OUTSIDE_TILE_VERTICAL TILE_EMPTY
+
+enum Tile {
+    TILE_EMPTY = ' ',
+    TILE_ZERO = '\0',
+    TILE_FULL = '#'
+};
+
+typedef uint8_t Tilemap[TILEMAP_SIZE_X][TILEMAP_SIZE_Y];
+
+bool tileIsEmpty(Tile tile) {
+    switch (tile) {
+    case TILE_EMPTY:
+        return true;
+    case TILE_ZERO:
+        return true;
+    }
+    return false;
+}
+
+Tile tilemapGetTile(Tilemap* tilemap, int x, int y) {
+    if (x < 0 || x >= TILEMAP_SIZE_X) return OUTSIDE_TILE_HORIZONTAL;
+    if (y < 0 || y >= TILEMAP_SIZE_Y) return OUTSIDE_TILE_VERTICAL;
+    return (Tile)(*tilemap)[y][x];
+}
+
+Tilemap levelTilemaps[] = {
+    {
+
+    },
+};
 
 int main() {
     // Initialization
+    Tilemap tilemap = {
+        "",
+        "ffg   asd"
+    };
 	
     const int screenWidth = 800;
     const int screenHeight = 450;
@@ -28,6 +69,16 @@ int main() {
 		ClearBackground(RED);
         DrawText("move the ball with arrow keys", 10, 10, 20, GRAY);
         DrawCircleV(ballPosition, 50, WHITE);
+
+        for (int x = 0; x < TILEMAP_SIZE_X; x++) {
+            for (int y = 0; y < TILEMAP_SIZE_Y; y++) {
+                Tile tile = (Tile)tilemap[y][x];
+                if (tile != TILE_EMPTY && tile != TILE_ZERO) {
+                    DrawRectangle(x * TILE_PIXELS, y * TILE_PIXELS, TILE_PIXELS, TILE_PIXELS, YELLOW);
+                }
+            }
+        }
+
         EndDrawing();
     }
 
